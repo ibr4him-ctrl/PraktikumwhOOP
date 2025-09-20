afe5854bd73b10eb1e450ba3d273e63f3fbec47c
ibr4him-ctrl <ibrahimsteik24@gmail.com>
2025-09-20 14:16:57 +0700

    nambah harapan

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, World!\n");
+    System.out.println("pak riza keren, bismillah OOP A\n");
 }
 }
