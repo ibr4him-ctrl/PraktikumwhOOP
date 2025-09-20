72de716064dcc4913ec4868e46dc570a0a898647
ibr4him-ctrl <ibrahimsteik24@gmail.com>
2025-09-20 17:17:49 +0700

    nambahin cita cita, doain ya

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello, World!\n");
     System.out.println("pak riza keren, bismillah OOP A");
+    System.out.println("aku, ibra. sang tiriliuner");
 }
 }
